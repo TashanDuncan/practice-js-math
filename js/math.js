0aad8c898fe8a23de37e05475c17b9757a16632a
viewtifult <tashanduncan1994@gmail.com>
2020-11-04 21:16:03 +0000

    practice complete

--- js/math.js
+++ js/math.js
@@ -1,12 +1,15 @@
 // 1. Attach this file -- math.js -- to the index.html file using a <script> tag
 
 // 2. Add an alert to announce the program with a message like "Let's do some math!"
-
+alert("Lets Do Some Math!");
 // 3. Create a variable and use the prompt() method to collect a number from a visitor
-
+const userNum1 = prompt('Please input a number');
 // 4. Convert that value from a string to a floating point number
+const num1 = +userNum1;
 
 // 5. Repeat steps 3 and 4 to create a second variable and collect a second number
+const userNum2 = prompt('Please input another number');
+const num2 = +userNum2;
 
 // 6. Create a new variable -- message -- which you'll use to build
 //    a complete message to print to the document
@@ -16,7 +19,11 @@
 //    the values input from the user. Use string concatenation to create this
 //    and make sure you actually perform the math on the values by 
 //    using the + symbol to add their values together
-
+const message = `<h1>Math  with the numbers ${num1} and ${num2}</h1>` 
+                + `${num1} + ${num2} = ${num1 + num2}<br>` 
+                + `${num1} * ${num2} = ${num1 * num2}<br>`
+                + `${num1} / ${num2} = ${num1 / num2}<br>`
+                + `${num1} - ${num2} = ${num1 - num2}<br>`;
 // 7. Add another string to the message variable. 
 //    The string should look something like this after concatenation:
 //    "3 + 4 = 7"
@@ -33,7 +40,7 @@
 // 10. Use the document.write() method to print the message variable 
 //     to the web page. Open the finished.png file in this workspace
 //     to see what the completed output should look like
-
+document.write(message)
 
 
 
